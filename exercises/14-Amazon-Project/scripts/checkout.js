c09b5c37af70dcec3606e55031506af7173c2f40
Iqra327 <iqrarasheed219@gmail.com>
2024-07-09 22:55:46 +0500

    initial-commit

--- exercises/14-Amazon-Project/scripts/checkout.js
+++ exercises/14-Amazon-Project/scripts/checkout.js
@@ -42,7 +42,7 @@ cart.forEach((cartItem)=>{
               Update
             </span>
             <input class="quantity-input">
-            <span class="save-quantity-link link-primary">Save</span>
+            <span class="save-quantity-link link-primary js-save-quantity" data-product-id = "${matchingProduct.id}">Save</span>
             <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
               Delete
             </span>
@@ -126,7 +126,23 @@ document.querySelectorAll('.js-delete-link')
   document.querySelectorAll('.js-update-link').forEach((link) => {
     link.addEventListener('click', () => {
       const productId = link.dataset.productId;
-      console.log(productId);
+
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
+      container.classList.add('is-editing-quantity');
     });
     
   });
+
+  document.querySelectorAll('.js-save-quantity').forEach((link) => {
+    link.addEventListener('click', () => {
+      const productId = link.dataset.productId;
+
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
+      container.classList.remove('is-editing-quantity');
+
+    })
+  })
